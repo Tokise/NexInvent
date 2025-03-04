89a389b870d4f6c058c9077cf8facee622cc1a42
Rei. <ablenjohnrey@gmail.com>
2025-03-04 15:23:00 +0800

    Update db.php

--- src/config/db.php
+++ src/config/db.php
@@ -1,6 +1,6 @@
 <?php
 // Database configuration
-define('DB_HOST', 'http://192.168.254.114:8080');
+define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');  // Default XAMPP MySQL password is blank
 define('DB_NAME', 'nexinvent');
